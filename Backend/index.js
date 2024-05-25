dae65ada609b02281495827cc35311dd12f09484
amitVaibhavTiwari <amitvaibhavtiwari@gmail.com>
2024-05-25 14:07:39 +0530

    added cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 // middleware to handle cors policy below
 app.use(
   cors({
-    origin: "*",
+    origin: "https://good-reads-eight.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
